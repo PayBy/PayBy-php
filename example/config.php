39e870d59fee8b9f4015d5309d37bf2a2aa7dcab
pansmaze <240457063@qq.com>
2020-05-23 23:00:21 +0800

    First commit, add framework & example
    Add PayBy PHP SDK framework & example

--- example/config.php
+++ example/config.php
@@ -3,7 +3,7 @@
 // payby partner id
 const PARTNER_ID = '200000000001';
 
-\PayBy\PayBy::setParterId(PARTNER_ID); // 设置 App ID
+\PayBy\PayBy::setParterId(PARTNER_ID);
 
 \PayBy\PayBy::setPrivateKey(file_get_contents(__DIR__ . '/PayBy_key_private.pem'));
 \PayBy\PayBy::setPublicKey(file_get_contents(__DIR__ . '/PayBy_key_public_key.pem'));
